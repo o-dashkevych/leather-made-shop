82e078b97879ea109d80cd826f9b7fac7d5c2a39
Oleh Dashkevych <oleh_dashkevych@epam.com>
2016-05-13 14:35:02 +0300

    Made email validation and repeat email validation in JS.

--- web-side/src/main/webapp/WEB-INF/js/validation.js
+++ web-side/src/main/webapp/WEB-INF/js/validation.js
@@ -1,27 +1,51 @@
-var emailInput = document.getElementById('email');
-if (emailInput.addEventListener) { // For all major browsers, except IE 8 and earlier
-    emailInput.addEventListener("oninput", emailValidation);
-} else if (emailInput.attachEvent) { // For IE 8 and earlier versions
-    emailInput.attachEvent("oninput", emailValidation);
-}
+attachEventToElement("email", "input", setWarningIfInvalidEmail);
+attachEventToElement("emailRepeat", "input", setWarningIfInvalidRepeatEmail);
 
-var emailValidation = function validateEmail() {
-    var emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-    var email = document.getElementsByName('email')[0].value
-    var result = emailPattern.test(email);
-    console.log('matches = ' + result);
-    var warningEmailId = 'warningEmail';
-    var warning = createEmailWarningIfNotExistAndGet(warningEmailId);
-    if (!result) {
-        warning.textContent = 'Invalid email';
-    } else if (isExist(warningEmailId)) {
-        document.getElementById(warningEmailId).textContent = '';
+function attachEventToElement(elementId, eventName, functionName) {
+    var element = document.getElementById(elementId);
+    if (element.addEventListener) { // For all major browsers, except IE 8 and earlier
+        element.addEventListener(eventName, functionName);
+    } else if (element.attachEvent) { // For IE 8 and earlier versions
+        element.attachEvent("on" + eventName, functionName);
     }
 }
 
-function createEmailWarning(id) {
+function setWarningIfInvalidRepeatEmail() {
+    var isValidRepeatEmail = validateRepeatEmail("email", "emailRepeat");
+    console.log('are same emails = ' + isValidRepeatEmail);
+    setWarningTextIfInvalid("emailRepeatDiv", "warningRepeatEmail", "Emails doesn't match", isValidRepeatEmail)
+}
+
+function validateRepeatEmail(firstEmailField, secondEmailField) {
+    var firstEmail = document.getElementsByName(firstEmailField)[0].value;
+    var secondEmail = document.getElementsByName(secondEmailField)[0].value;
+    return firstEmail === secondEmail;
+}
+
+function setWarningIfInvalidEmail() {
+    var isValidEmail = validateEmail('email');
+    console.log('is valid email = ' + isValidEmail);
+    setWarningTextIfInvalid("emailDiv", "warningEmail", "Invalid email", isValidEmail)
+}
+
+function setWarningTextIfInvalid(divId, warningId, warning, isValid) {
+    var warningElement = createElementWarningIfNotExistAndGet(divId, warningId);
+    if (!isValid) {
+        warningElement.textContent = warning;
+    } else if (isExist(warningId)) {
+        document.getElementById(warningId).textContent = '';
+    }
+}
+
+function validateEmail(name) {
+    var emailPattern = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+    var email = document.getElementsByName(name)[0].value;
+    return emailPattern.test(email);
+}
+
+function createWarning(fieldDiv, id) {
     var warning = document.createElement('p');
-    var emailDiv = document.getElementById('emailDiv');
+    var emailDiv = document.getElementById(fieldDiv);
     warning.setAttribute('id', id);
     emailDiv.appendChild(warning);
     return warning;
@@ -33,11 +57,11 @@ function isExist(id) {
     return result;
 }
 
-function createEmailWarningIfNotExistAndGet(id) {
+function createElementWarningIfNotExistAndGet(fieldDiv, id) {
     if (isExist(id)) {
         var resultItem = document.getElementById(id);
     } else {
-        resultItem = createEmailWarning(id);
+        resultItem = createWarning(fieldDiv, id);
     }
     return resultItem;
 }
