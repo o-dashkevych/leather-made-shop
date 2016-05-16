e1edd259c02a1af9159507649435e01be9817551
Oleh Dashkevych <oleh_dashkevych@epam.com>
2016-05-16 22:01:17 +0300

    Set up servlet configs and run.

---
+++ web-side/src/main/java/SimpleServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Oleh_Dashkevych.
+ */
+@WebServlet(urlPatterns = {"/home"})
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/index.html").forward(req, resp);
+    }
+}
